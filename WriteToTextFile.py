4cf7db87ac69aae73b782eae2728394a1199ad4a
Swinubo <123121161+Swinubo@users.noreply.github.com>
2023-03-22 20:51:57 -0400

    Delete WriteToTextFile.py

--- WriteToTextFile.py
+++
@@ -1,7 +0,0 @@
-import easygui
-
-Name = easygui.enterbox("What is your account name?")
-
-with open("C:\\Users\\fmend\\Documents\\Gaspar\\Python Projects\\Accounts.txt", "a") as Accounts:
-    Accounts.write("\n" + Name)
-    Accounts.close()
